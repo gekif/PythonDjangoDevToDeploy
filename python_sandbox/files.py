0e32181a41bf60c1bd752337663eb25c4bb9a1f5
Gekif <dzulfikar.maulana@gmail.com>
2020-05-01 13:38:25 +0700

    3. Python Language Fundamentals (Optional): 11. Files

--- python_sandbox/files.py
+++ python_sandbox/files.py
@@ -1,1 +1,24 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open('myfile.txt', 'w')
+
+# Get some info
+print('Name:', myFile.name)
+print('Is Closed:', myFile.closed)
+print('Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and javascript')
+myFile.close()
+
+# Append to fike
+myFile = open('myfile.txt', 'a')
+myFile.write(' I also like PHP')
+myFile.close()
+
+# Read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
