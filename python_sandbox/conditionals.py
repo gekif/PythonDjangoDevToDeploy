5da7ff533094a1aed99874e60a8566a6ef0cdef9
Gekif <dzulfikar.maulana@gmail.com>
2020-05-01 12:09:15 +0700

    3. Python Language Fundamentals (Optional): 7. Conditionals

--- python_sandbox/conditionals.py
+++ python_sandbox/conditionals.py
@@ -1,17 +1,69 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
+x = 10
+y = 10
 
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+# Simple if
+if x == y:
+    print(f'{x} is equal to {y}')
+
+# if/else
+# if x > y:
+#     print(f'{x} is greater than {y}')
+# else:
+#     print(f'{x} is less than {y}')
+
+# elif
+# if x > y:
+#     print(f'{x} is greater than {y}')
+# elif x == y:
+#     print(f'{x} is equal to {y}')
+# else:
+#     print(f'{x} is less than {y}')
+
+# Nested if
+# if x > 2:
+#     if x <= 10:
+#         print(f'{x} is less than 2 and greater than 10')
 
 
 # Logical operators (and, or, not) - Used to combine conditional statements
 
+# and
+# if x > 2 and x <= 10:
+#     print(f'{x} is less than 2 and greater than 10')
 
+# or
+# if x > 2 or x <= 10:
+#     print(f'{x} is less than 2 and greater than 10')
+
+# not
+# if not(x == y):
+#     print(f'{x} is not equal to {y}')
 
 
 # Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
+
+numbers = [1 ,2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# in
+# if x in numbers:
+#     print(x in numbers)
+
+# not in
+# if x not in numbers:
+#     print(x in numbers)
+
+# is
+# if x is y:
+#     print(x is y)
+
+# is not
+if x is not y:
+    print(x is not y)
 
 
 
