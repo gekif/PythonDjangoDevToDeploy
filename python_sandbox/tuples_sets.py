56114190b8ed63fe9c66226631f1358b9e64556c
Gekif <dzulfikar.maulana@gmail.com>
2020-05-01 10:59:29 +0700

    3. Python Language Fundamentals (Optional): 4. Tuples & Sets

--- python_sandbox/tuples_sets.py
+++ python_sandbox/tuples_sets.py
@@ -1,5 +1,47 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
+
+# Simple tuple
+# fruit_tuple = ('Apple', 'Orange', 'Mango')
+
+# Using constructor
+fruit_tuple = tuple(('Apple', 'Orange', 'Mango'))
+
+# Get single value
+# print(fruit_tuple[1])
+
+# Can not change value
+# fruit_tuple[1] = 'Grape'
+
+# Tuples with one value should have trailing comma
+fruit_tuple_2 = ('Apple',)
+# print(fruit_tuple_2)
+
+# Get length of tuple
+print(len(fruit_tuple))
+print(fruit_tuple)
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create set
+fruit_set = {'Apple', 'Orange', 'Mango', 'Apple'}
+# print(fruit_set)
+
+# Check if in set
+print('Apple' in fruit_set)
+
+# Add to set
+fruit_set.add('Grape')
+print(fruit_set)
+
+# Remove from set
+fruit_set.remove('Grape')
+print(fruit_set)
+
+# Clear set
+fruit_set.clear()
+print(fruit_set)
+
+# Delete set
+del fruit_set
+print(fruit_set)
