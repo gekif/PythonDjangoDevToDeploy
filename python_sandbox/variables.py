0bcdd7249fca3f1f57c1063edd79fc0f58470eeb
Gekif <dzulfikar.maulana@gmail.com>
2020-05-01 10:30:10 +0700

    3. Python Language Fundamentals (Optional): 1. Variables & Data Types

--- python_sandbox/variables.py
+++ python_sandbox/variables.py
@@ -13,3 +13,29 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+# x = 1           # int
+# y = 2.5         # float
+# name = 'Agus'   # string
+# is_cool = True  # bool
+
+# Multiple Assignment
+x, y, name, is_cool = (2, 3.5, 'Larry', False)
+
+print(x, y, name, is_cool)
+
+
+# Basic Math
+a = x + y
+
+print (a)
+
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+# Check Type
+print(type(z))
+print(z)
