5fe093be83b6ec3235c6fa103d32ab03e4345f3e
Gekif <dzulfikar.maulana@gmail.com>
2020-05-01 10:48:47 +0700

    3. Python Language Fundamentals (Optional): 3. Lists

--- python_sandbox/lists.py
+++ python_sandbox/lists.py
@@ -1,1 +1,48 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create lits
+# numbers = [1, 2, 3, 4, 5]
+
+# Using a constructor
+numbers = list((1, 2, 3, 4, 5))
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+
+# print(type(numbers))
+# print(numbers)
+
+print(fruits)
+
+# Get Value
+print(fruits[1])
+
+# Get len
+print(len(fruits))
+
+# Append to list
+fruits.append('Mangos')
+print(fruits)
+
+# Remove from list
+fruits.remove('Grapes')
+print(fruits)
+
+# Insert into position
+fruits.insert(2, 'Strawberry')
+print(fruits)
+
+# Remove from position
+fruits.pop(3)
+print(fruits)
+
+# Reverse list
+fruits.reverse()
+print(fruits)
+
+# Sort list
+fruits.sort()
+print(fruits)
+
+# Reverse sort
+fruits.sort(reverse=True)
+print(fruits)
